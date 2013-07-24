265f2bf968e937de524bf73979042150dd544597
Craig Meinschein <cmeinsch@gmail.com>
2013-07-24 11:37:08 -0700

    Fixed up whitespace.

--- Create-SCCMPackage.ps1
+++ Create-SCCMPackage.ps1
@@ -1,47 +1,46 @@
 param(
     [CmdletBinding()]
-	[Parameter(Mandatory=$true)]
+    [Parameter(Mandatory=$true)]
     [Alias('Name')]
     [String]$SoftwareName,
     [Parameter(Mandatory=$false)]
-	[String]$Version,
+    [String]$Version,
     [Parameter(Mandatory=$false)]
     [Alias('Vendor')]
-	[String]$Manufacturer,
+    [String]$Manufacturer,
     [Parameter(Mandatory=$false)]
-	[String[]]$InstallTypes = $null,
+    [String[]]$InstallTypes = $null,
     [Parameter(Mandatory=$false)]
-	[String[]]$UninstallTypes = $null
+    [String[]]$UninstallTypes = $null
 )
 
 # Load necessary Modules.
 Import-Module ActiveDirectory
 Import-Module "C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\ConfigurationManager.psd1"
-
 $GroupOU = "OU=SCCM Applications,OU=CECS Groups,DC=DS,DC=CECS,DC=PDX,DC=EDU"
 
 # Create Install group(s)
 if ($InstallTypes) {
-	foreach ($Type in $InstallTypes) {
-		$GroupName = "SCCM_${SoftwareName} ${Version} ${Type}"
-		Write-Host "Creating group '$GroupName'."
-		New-ADGroup $GroupName -DisplayName $GroupName -Path $GroupOU -GroupScope Global
-	}
+    foreach ($Type in $InstallTypes) {
+        $GroupName = "SCCM_${SoftwareName} ${Version} ${Type}"
+        Write-Host "Creating group '$GroupName'."
+        New-ADGroup $GroupName -DisplayName $GroupName -Path $GroupOU -GroupScope Global
+    }
 } else {
-	$GroupName = "SCCM_${SoftwareName} ${Version}"
-	Write-Host "Creating group '$GroupName'."
-	New-ADGroup $GroupName -DisplayName $GroupName -Path $GroupOU -GroupScope Global
+    $GroupName = "SCCM_${SoftwareName} ${Version}"
+    Write-Host "Creating group '$GroupName'."
+    New-ADGroup $GroupName -DisplayName $GroupName -Path $GroupOU -GroupScope Global
 }
 
 # Create Uninstall group(s)
 if ($UninstallTypes) {
-	foreach ($Type in $UninstallTypes) {
-		$GroupName = "SCCM_${SoftwareName} ${Version} ${Type} Uninstall"
-		Write-Host "Creating group '$GroupName'."
-		New-ADGroup $GroupName -DisplayName $GroupName -Path $GroupOU -GroupScope Global
-	}
+    foreach ($Type in $UninstallTypes) {
+        $GroupName = "SCCM_${SoftwareName} ${Version} ${Type} Uninstall"
+        Write-Host "Creating group '$GroupName'."
+        New-ADGroup $GroupName -DisplayName $GroupName -Path $GroupOU -GroupScope Global
+    }
 } else {
-	$GroupName = "SCCM_${SoftwareName} ${Version} Uninstall"
-	Write-Host "Creating group '$GroupName'."
-	New-ADGroup $GroupName -DisplayName $GroupName -Path $GroupOU -GroupScope Global
+    $GroupName = "SCCM_${SoftwareName} ${Version} Uninstall"
+    Write-Host "Creating group '$GroupName'."
+    New-ADGroup $GroupName -DisplayName $GroupName -Path $GroupOU -GroupScope Global
 }
