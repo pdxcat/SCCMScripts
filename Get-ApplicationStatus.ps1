60ba3800e52407c9b6f9a2ad4f8f89e4236616d4
Craig Meinschein <cmeinsch@gmail.com>
2015-02-17 17:52:19 -0800

    Begun work on script to get SCCM Application status.

---
+++ Get-ApplicationStatus.ps1
@@ -0,0 +1,22 @@
+﻿param(
+    [String]$ComputerName = $env:computername
+)
+Function Set-DefaultProperties {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true,ValueFromPipeline=$True)][Object[]]$InputObject,
+        [Parameter(Mandatory=$true)][String[]]$Properties,
+        [Switch]$PassThru
+    )
+    BEGIN {
+        $PropSet = New-Object System.Management.Automation.PSPropertySet('DefaultDisplayPropertySet',[string[]]$Properties)
+        $PSStandardMembers = [System.Management.Automation.PSMemberInfo[]]@($PropSet)
+    }
+    PROCESS {
+        $InputObject | Add-Member -MemberType MemberSet -Name PSStandardMembers -Value $PSStandardMembers
+        if ($PassThru) { Write-Output $InputObject }
+    }
+}
+$Apps = Get-WmiObject -Namespace "Root\CCM\ClientSDK" -Class CCM_Application
+$Apps | Set-DefaultProperties -Properties FullName,SoftwareVersion,Publisher,ApplicabilityState,SupersessionState,InstallState
+$Apps
